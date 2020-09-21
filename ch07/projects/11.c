00b8f22a156f5509d719f46cf895e4624ef15c14
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-09-21 16:41:45 +0700

    ✔ finish chapter 7

--- ch07/projects/11.c
+++ ch07/projects/11.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 int main()
 {
+  int i = 0;
+  bool is_scanning_last = false;
+  char cur, *first, *last;
+
+  first = malloc(50 * sizeof(char));
+  last = malloc(50 * sizeof(char));
+
+  printf("Enter a first and last name: ");
+  for (;;)
+  {
+    cur = getchar();
+    if (cur != '\n')
+    {
+      if (isspace(cur))
+      {
+        i = 0;
+        is_scanning_last = true;
+      }
+      else
+      {
+        if (is_scanning_last)
+        {
+          last[i++] = cur;
+        }
+        else
+        {
+          first[i++] = cur;
+        }
+      }
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  printf("%s, %c.\n", last, first[0]);
+  return 0;
 }
--- ch07/projects/15.c
+++ ch07/projects/15.c
@@ -1,5 +1,108 @@
 #include <stdio.h>
+#include <stdint.h>
+
+short fact_short(int *const n);
+int fact_int(int *const n);
+long fact_long(int *const n);
+long long fact_long_long(int *const n);
+float fact_float(int *const n);
+double fact_double(int *const n);
+long double fact_long_double(int const *n);
 
 int main()
 {
+  int inp;
+
+  printf("Enter a positive integer: ");
+  scanf("%d", &inp);
+
+  printf("Factorial of %d as ...\n", inp);
+  printf("`short`:\t\t%hd\n", fact_short(&inp));
+  printf("`int`:\t\t%d\n", fact_int(&inp));
+  printf("`long`:\t\t%ld\n", fact_long(&inp));
+  printf("`long long`:\t%lld\n", fact_long_long(&inp));
+  printf("`float`:\t%f\n", fact_float(&inp));
+  printf("`double`:\t%lf\n", fact_double(&inp));
+  printf("`long double`:\t%Lf\n", fact_long_double(&inp));
+
+  return 0;
+}
+
+short fact_short(int *const n)
+{
+  short res = 1;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+int fact_int(int *const n)
+{
+  int res = 1;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+long fact_long(int *const n)
+{
+  long res = 1;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+long long fact_long_long(int *const n)
+{
+  long long res = 1;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+float fact_float(int *const n)
+{
+  float res = 1.0;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+
+double fact_double(int *const n)
+{
+  double res = 1.0;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+}
+long double fact_long_double(int const *n)
+{
+  long double res = 1.0;
+
+  for (int i = 1; i <= *n; i++)
+  {
+    res *= i;
+  }
+
+  return res;
 }
