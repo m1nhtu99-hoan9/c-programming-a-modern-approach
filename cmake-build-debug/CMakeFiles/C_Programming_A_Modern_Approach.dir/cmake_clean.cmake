ec7a63f73b2f634ea1715e622c8ae45c9a71a2f3
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-09-27 18:28:58 +0700

    doing chapter 08

---
+++ cmake-build-debug/CMakeFiles/C_Programming_A_Modern_Approach.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Programming_A_Modern_Approach.dir/ch08/projects/09.c.o"
+  "C_Programming_A_Modern_Approach"
+  "C_Programming_A_Modern_Approach.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Programming_A_Modern_Approach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
